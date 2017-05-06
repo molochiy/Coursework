66f0d75b787c88b7fe052b4ce3785c76818dac4a
Mykola Molochiy <Mykola Molochiy>
2017-05-06 14:30:32 +0300

    Added index page

---
+++ src/Coursework.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Optimization;
+
+namespace Coursework.Web
+{
+  public class BundleConfig
+  {
+    public static void RegisterBundles(BundleCollection bundles)
+    {
+      bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/css/custom/common.css"));
+
+      BundleTable.EnableOptimizations = false;
+    }
+  }
+}
