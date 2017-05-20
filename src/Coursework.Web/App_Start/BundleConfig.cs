d50da96c0ccb15a186b97a5f71fdec4fbf5e860a
kopylets2580 <kopylets2580.gmail.com>
2017-05-20 23:41:53 +0300

    added derectiver for problems forms

--- src/Coursework.Web/App_Start/BundleConfig.cs
+++ src/Coursework.Web/App_Start/BundleConfig.cs
@@ -10,7 +10,7 @@ namespace Coursework.Web
 
       bundles.Add(new ScriptBundle("~/bundles/problems").Include("~/Scripts/app/pages/problems/problems.module.js", "~/Scripts/app/pages/problems/problems.controller.js"));
 
-      bundles.Add(new StyleBundle("~/bundles/css").Include("~/Content/css/custom/common.css", "~/Content/css/custom/user.css", "~/Content/css/custom/menu.css", "~/Content/css/custom/flexslider.css"));
+      bundles.Add(new StyleBundle("~/bundles/css").Include("~/Content/css/custom/common.css", "~/Content/css/custom/user.css", "~/Content/css/custom/menu.css", "~/Content/css/custom/flexslider.css", "~/Content/css/lib/bootstrap.min.css"));
 
       bundles.Add(new ScriptBundle("~/bundles/flexslider").Include("~/Scripts/libs/flexslider/jquery.flexslider.js")); 
 
