c5dbbbc039c3ce87e7cc62c152b244c47105cc09
kopylets2580 <kopylets2580.gmail.com>
2017-05-29 23:38:52 +0300

    added arrays to type of first problem

--- src/Coursework.Services/Algorithms/AntennasRadiationPatternProblemAlgorithmType2.cs
+++ src/Coursework.Services/Algorithms/AntennasRadiationPatternProblemAlgorithmType2.cs
@@ -7,107 +7,27 @@ namespace Coursework.Services.Algorithms
 {
   static class AntennasRadiationPatternProblemAlgorithmType2
   {
-
     #region Constants
     static int M = 5;
     static int N = 5;
-    static double c1 = 1;
-    static double c2 = 1;
-    static double alpha = ((2 * Math.PI) * (2 * Math.PI)) / (c1 * c2);
-    static ComplexNumbers P = new ComplexNumbers(1, 0);
-    static double eps = 1e-6;
-    const int SplitNumberForIntegral = 20;
+    static double c1 = 1.15;
+    static double c2 = 1.15;
+    static double eps = 0.00001;
+    //const double alpha = ((2 * Math.PI) * (2 * Math.PI)) / (c1 * c2);
+    //static ComplexNumbers P = new ComplexNumbers(1, 0);
+    //static double eps = 1e-6;
+    const int SplitNumberForIntegral = 10;
     static double[] ksi1;
     static double[] ksi2;
-    static List<List<ComplexNumbers>> Imn = new List<List<ComplexNumbers>>();
-    static List<List<ComplexNumbers>> ImnNext = new List<List<ComplexNumbers>>();
-    static double Beta;
+    static ComplexNumbers P = new ComplexNumbers(1, 0);
+
+    static List<List<ComplexNumbers>> matrixF = new List<List<ComplexNumbers>>();
+    static List<List<ComplexNumbers>> matrixFNext = new List<List<ComplexNumbers>>();
+    //static double Beta;
+    static double modulF = 1;
+    static double argF = 0;
     #endregion
 
-    /// <summary>
-    /// calculate kernel 'K' in formula
-    /// </summary>
-    /// <param name="prevKsi1"></param>
-    /// <param name="prevKsi2"></param>
-    /// <param name="nextKsi1"></param>
-    /// <param name="nextKsi2"></param>
-    /// <returns></returns>
-    public static ComplexNumbers K(double prevKsi1, double prevKsi2, double nextKsi1, double nextKsi2)
-    {
-      ComplexNumbers sum = new ComplexNumbers();
-      for (int n = -N; n <= N; n++)
-      {
-        for (int m = -M; m <= M; m++)
-        {
-          sum += new ComplexNumbers
-          {
-            Re = Math.Cos(c1 * n * (nextKsi1 - prevKsi1) + c2 * m * ((nextKsi2 - prevKsi2))),
-            Im = Math.Sin(c1 * n * (nextKsi1 - prevKsi1) + c2 * m * ((nextKsi2 - prevKsi2)))
-          };
-        }
-      }
-      return sum;
-    }
-
-    public static void CopyImn(List<List<ComplexNumbers>> From, List<List<ComplexNumbers>> To)
-    {
-      for (int i = 0; i < 2*N+1; i++)
-      {
-        for (int j = 0; j < 2*M+1; j++)
-        {
-          To[i][j] = From[i][j];
-        }
-      }
-    }
-
-    /// <summary>
-    /// initialization current strength.
-    /// Set first values for current
-    /// </summary>
-    public static void InitializationImn()
-    {
-      Imn.Clear();
-      for (int i = 0; i < 2 * N + 1; i++)
-      {
-        Imn.Add(new List<ComplexNumbers>());
-        for (int j = 0; j < 2 * M + 1; j++)
-        {
-          Imn[i].Add(new ComplexNumbers(1.0, 0));
-        }
-      }
-    }
-
-    public static void InitializationImnNext()
-    {
-      ImnNext.Clear();
-      for (int i = 0; i < 2 * N + 1; i++)
-      {
-        ImnNext.Add(new List<ComplexNumbers>());
-        for (int j = 0; j < 2 * M + 1; j++)
-        {
-          ImnNext[i].Add(new ComplexNumbers());
-        }
-      }
-    }
-
-    public static bool checkForСonvergence(List<List<ComplexNumbers>> FirstList, List<List<ComplexNumbers>> SecondList)
-    {
-      bool flag = true;
-
-      for (int n = 0; n < 2 * N + 1; ++n)
-      {
-        for (int m = 0; m < 2 * M + 1; ++m)
-        {
-          if ((FirstList[n][m] - SecondList[n][m]).Module() > eps)
-            flag = false;
-        }
-      }
-      return flag;
-    }
-
-    /// <summary>
-    /// set splitting for ksi1 and ksi2 for calculate integral
-    /// </summary>
     public static void Splitting()
     {
       ksi1 = new double[SplitNumberForIntegral + 1];
@@ -119,213 +39,219 @@ namespace Coursework.Services.Algorithms
       }
     }
 
-    /// <summary>
-    /// set function of two variables
-    /// write your function for calculate integral later
-    /// </summary>
-    /// <param name="i"></param>
-    /// <param name="j"></param>
-    /// <returns></returns>
-    public static ComplexNumbers Fxy(int i, int j, int n, int m)
+    public static ComplexNumbers GetK(double integralKsi1, double integralKsi2, double Ksi1, double Ksi2)
     {
-      double x = ksi1[i];
-      double y = ksi2[j];
-
-      ComplexNumbers resultFunction = new ComplexNumbers(P);
-      resultFunction *= new ComplexNumbers(Math.Cos(c1 * n * x + c2 * m * y), -1 * Math.Sin(c1 * n * x + c2 * m * y));
-
-      ComplexNumbers summ = new ComplexNumbers();
-      for (int k = -N; k <= N; ++k)
+      ComplexNumbers sum = new ComplexNumbers();
+      for (int n = -N; n <= N; n++)
       {
-        for (int l = -M; l <= M; ++l)
+        for (int m = -M; m <= M; m++)
         {
-          summ += Imn[k + N][l + M] * new ComplexNumbers(Math.Cos(c1 * l * x + c2 * k * y), Math.Sin(c1 * l * x + c2 * k * y));
+          double val = c1 * n * (Ksi1 - integralKsi1) + c2 * m * ((Ksi2 - integralKsi2));
+          sum += new ComplexNumbers
+          {
+            Re = Math.Cos(val),
+            Im = Math.Sin(val)
+          };
+        }
+      }
+      return sum;
+    }
+
+    public static void InitializeF()
+    {
+      matrixF = new List<List<ComplexNumbers>>();
+      for (int i = 0; i <= SplitNumberForIntegral; i++)
+      {
+        matrixF.Add(new List<ComplexNumbers>());
+        for (int j = 0; j <= SplitNumberForIntegral; j++)
+        {
+          matrixF[i].Add(new ComplexNumbers
+          {
+            Re = modulF * Math.Cos(argF),
+            Im = modulF * Math.Sin(argF)
+          });
+        }
+      }
+    }
+
+    public static void CopyMatrix(List<List<ComplexNumbers>> From, List<List<ComplexNumbers>> To)
+    {
+      for (int i = 0; i < From.Count; i++)
+      {
+        for (int j = 0; j < From[i].Count; j++)
+        {
+          To[i][j] = From[i][j];
+        }
+      }
+    }
+
+    public static double getAlpha()
+    {
+      return Math.Pow(2 * Math.PI, 2) / (c1 * c2);
+    }
+
+    public static void GetNextF()
+    {
+      if (matrixFNext.Count != 0) CopyMatrix(matrixFNext, matrixF);
+      NormalizeF();
+      matrixFNext = new List<List<ComplexNumbers>>();
+      for (int i = 0; i <= SplitNumberForIntegral; i++)
+      {
+        matrixFNext.Add(new List<ComplexNumbers>());
+        for (int j = 0; j <= SplitNumberForIntegral; j++)
+        {
+          matrixFNext[i].Add(new ComplexNumbers(1.0 / (2 * Math.Pow(getAlpha(), 2)), 0) * CalculateIntegral(ksi1[i], ksi2[j]));
+        }
+      }
+    }
+
+    public static ComplexNumbers GetF(int i, int j)
+    {
+      return matrixF[i][j];
+    }
+
+    public static ComplexNumbers GetValueUnerIntegralFunction(int i, int j, double Ksi1, double Ksi2)
+    {
+      double integralKsi1 = ksi1[i];
+      double integralKsi2 = ksi2[j];
+      return P * GetK(integralKsi1, integralKsi2, Ksi1, Ksi2) * GetF(i, j);
+    }
+
+    public static void NormalizeF()
+    {
+      for (int i = 0; i < matrixF.Count; i++)
+      {
+        for (int j = 0; j < matrixF[i].Count; j++)
+        {
+          matrixF[i][j].Re = Math.Abs(matrixF[i][j].Re);
         }
       }
 
-      resultFunction *= summ;
+      ComplexNumbers maxF = new ComplexNumbers(matrixF[0][0]);
+      for (int i = 0; i < matrixF.Count; i++)
+      {
+        for (int j = 0; j < matrixF[i].Count; j++)
+        {
+          if (matrixF[i][j].Re > maxF.Re) maxF.Re = matrixF[i][j].Re;
+        }
+      }
 
-      return resultFunction;
+      for (int i = 0; i < matrixF.Count; i++)
+      {
+        for (int j = 0; j < matrixF[i].Count; j++)
+        {
+          matrixF[i][j].Re /= maxF.Re;
+
+        }
+      }
+
     }
 
-    /// <summary>
-    /// calculate integral for function Fxy
-    /// </summary>
-    /// <returns></returns>
-    public static ComplexNumbers CalculateIntegral(int n, int m)
+    public static ComplexNumbers CalculateIntegral(double Ksi1, double Ksi2)
     {
-      int Nint = 10, Mint = 10;
+      //reshitka
       double a = -1, b = 1, c = -1, d = 1;
-      double Hx = (b - a) / (2 * Nint);
-      double Hy = (d - c) / (2 * Mint);
-      ComplexNumbers I = new ComplexNumbers(Hx * Hy / 9, 0);
+
+      double Hx = (b - a) / (2 * N);
+      double Hy = (d - c) / (2 * M);
+      ComplexNumbers I = new ComplexNumbers(Hx * Hy / 9.0, 0);
       ComplexNumbers sum = new ComplexNumbers();
 
-      for (int i = 0; i < Nint; i++)
+      for (int i = 0; i < SplitNumberForIntegral / 2; i++)
       {
-        for (int j = 0; j < Mint; j++)
+        for (int j = 0; j < SplitNumberForIntegral / 2; j++)
         {
-          sum += Fxy(2 * i, 2 * j, n, m) + Fxy(2 * i + 2, 2 * j, n, m) + Fxy(2 * i, 2 * j + 2, n, m) + Fxy(2 * i + 2, 2 * j + 2, n, m) +
-              4 * Fxy(2 * i + 1, 2 * j, n, m) + 4 * Fxy(2 * i, 2 * j + 1, n, m) + 4 * Fxy(2 * i + 2, 2 * j + 1, n, m) + 4 * Fxy(2 * i + 1, 2 * j + 2, n, m) +
-              16 * Fxy(2 * i + 1, 2 * j + 1, n, m);
+          sum += GetValueUnerIntegralFunction(2 * i, 2 * j, Ksi1, Ksi2) + GetValueUnerIntegralFunction(2 * i + 2, 2 * j, Ksi1, Ksi2) +
+              GetValueUnerIntegralFunction(2 * i, 2 * j + 2, Ksi1, Ksi2) + GetValueUnerIntegralFunction(2 * i + 2, 2 * j + 2, Ksi1, Ksi2) +
+              4 * GetValueUnerIntegralFunction(2 * i + 1, 2 * j, Ksi1, Ksi2) + 4 * GetValueUnerIntegralFunction(2 * i, 2 * j + 1, Ksi1, Ksi2) +
+              4 * GetValueUnerIntegralFunction(2 * i + 2, 2 * j + 1, Ksi1, Ksi2) + 4 * GetValueUnerIntegralFunction(2 * i + 1, 2 * j + 2, Ksi1, Ksi2) +
+              16 * GetValueUnerIntegralFunction(2 * i + 1, 2 * j + 1, Ksi1, Ksi2);
         }
       }
 
       return I * sum;
     }
 
-    /// <summary>
-    /// calculate beta value
-    /// </summary>
-    /// <returns></returns>
-    public static void CalculateBeta()
+    public static bool checkForСonvergence(List<List<ComplexNumbers>> FirstList, List<List<ComplexNumbers>> SecondList)
     {
-      double result = 0.0;
-      for (int n = 0; n < 2*N+1; ++n)
+      bool flag = true;
+
+      for (int n = 0; n <= 10; ++n)
       {
-        for (int m = 0; m < 2 * M + 1; ++m)
+        for (int m = 0; m <= 10; ++m)
         {
-          result += Math.Pow(Imn[n][m].Module(), 2);
+          if ((FirstList[n][m] - SecondList[n][m]).Module() > eps)
+            flag = false;
         }
       }
-
-      Beta = result;
+      return flag;
     }
 
-    /// <summary>
-    /// searching Imn
-    /// </summary>
-    public static void CalculateImn()
-    {
-      Splitting();
-      InitializationImn();
-      InitializationImnNext();
-      int count = 0;
-      while (true)
-      {
-        CalculateBeta();
-        for (int n = 0; n < 2 * N + 1; ++n)
-        {
-          for (int m = 0; m < 2 * M + 1; ++m)
-          {
-            ImnNext[n][m] = CalculateIntegral(n, m);
-            ImnNext[n][m] = new ComplexNumbers(ImnNext[n][m].Re / (alpha * alpha * Beta), ImnNext[n][m].Im / (alpha * alpha * Beta));
-          }
-        }
+    public static List<List<ComplexNumbers>> Inm = new List<List<ComplexNumbers>>();
 
-        if (count == 20)
-        {
-          PrintResultImn();
-          break;
-        }
-        CopyImn(ImnNext, Imn);
-        count++;
-      }
+    //find Inm
+    ///////////////////////////////////////////////////////////////////////////
+    public static ComplexNumbers GetValueUnerIntegralFunctionI(int i, int j, int n, int m)
+    {
+      double integralKsi1 = ksi1[i];
+      double integralKsi2 = ksi2[j];
+      return P * (new ComplexNumbers(Math.Cos(c1 * n * integralKsi1 + c2 * m * integralKsi2), -1 * Math.Sin(c1 * n * integralKsi1 + c2 * m * integralKsi2))) * GetF(i, j);
     }
 
-    public static void NormalizeImn()
+    public static ComplexNumbers CalculateIntegralI(int n, int m)
     {
-      for (int i = 0; i < 2 * N + 1; i++)
+      //reshitka
+      double a = -1, b = 1, c = -1, d = 1;
+
+      double Hx = (b - a) / (2 * N);
+      double Hy = (d - c) / (2 * M);
+      ComplexNumbers I = new ComplexNumbers(Hx * Hy / 9.0, 0);
+      ComplexNumbers sum = new ComplexNumbers();
+
+      for (int i = 0; i < SplitNumberForIntegral / 2; i++)
       {
-        for (int j = 0; j < 2 * M + 1; j++)
+        for (int j = 0; j < SplitNumberForIntegral / 2; j++)
         {
-          Imn[i][j].Re = Math.Abs(Imn[i][j].Re);
+          sum += GetValueUnerIntegralFunctionI(2 * i, 2 * j, n, m) + GetValueUnerIntegralFunctionI(2 * i + 2, 2 * j, n, m) +
+              GetValueUnerIntegralFunctionI(2 * i, 2 * j + 2, n, m) + GetValueUnerIntegralFunctionI(2 * i + 2, 2 * j + 2, n, m) +
+              4 * GetValueUnerIntegralFunctionI(2 * i + 1, 2 * j, n, m) + 4 * GetValueUnerIntegralFunctionI(2 * i, 2 * j + 1, n, m) +
+              4 * GetValueUnerIntegralFunctionI(2 * i + 2, 2 * j + 1, n, m) + 4 * GetValueUnerIntegralFunctionI(2 * i + 1, 2 * j + 2, n, m) +
+              16 * GetValueUnerIntegralFunctionI(2 * i + 1, 2 * j + 1, n, m);
         }
       }
 
-      List<List<ComplexNumbers>> tempList = new List<List<ComplexNumbers>>();
-      for (int i = 0; i < 2 * N + 1; i++)
-      {
-        tempList.Add(new List<ComplexNumbers>());
-        for (int j = 0; j < 2*M+1; j++)
-        {
-          tempList[i].Add(new ComplexNumbers());
-        }
-      }
-
-      ComplexNumbers maxImn = new ComplexNumbers(Imn[0][0]);
-      ComplexNumbers maxImnNext = new ComplexNumbers(ImnNext[0][0]);
-      for (int i = 0; i < 2 * N + 1; i++)
-      {
-        for (int j = 0; j < 2 * M + 1; j++)
-        {
-          if (Imn[i][j].Re > maxImn.Re) maxImn.Re = Imn[i][j].Re;
-        }
-      }
-
-      for (int i = 0; i < 2 * N + 1; i++)
-      {
-        for (int j = 0; j < 2 * M + 1; j++)
-        {
-          Imn[i][j].Re /= maxImn.Re;
-
-        }
-      }
-
+      return I * sum;
     }
 
-    static List<List<double>> F = new List<List<double>>();
-    public static void FindF()
+
+    public static void NormalizeI()
     {
-      for (int i = 0; i <= 21; i++)
+      for (int i = 0; i < Inm.Count; i++)
       {
-        F.Add(new List<double>());
-        for (int j = 0; j <= 21; j++)
+        for (int j = 0; j < Inm[i].Count; j++)
         {
-          F[i].Add(0);
+          Inm[i][j].Re = Math.Abs(Inm[i][j].Re);
         }
       }
 
-
-      int et1 = 0;
-      int et2 = 0;
-      for (double i = -1; i <= 1; i += 0.1)
+      ComplexNumbers maxI = new ComplexNumbers(Inm[0][0]);
+      for (int i = 0; i < Inm.Count; i++)
       {
-        et2 = 0;
-        for (double j = -1; j <= 1; j += 0.1)
+        for (int j = 0; j < Inm[i].Count; j++)
         {
-          double summ = 0;
-          for (int n = -5; n <= 5; n++)
-          {
-            for (int m = -5; m <= 5; m++)
-            {
-              summ += Imn[n + 5][m + 5].Re * Math.Cos(c1 * n * i + c2 * m * j);
-            }
-          }
-          F[et1][et2] = summ;
-          et2++;
-        }
-        et1++;
-      }
-    }
-
-    public static void NormalizeF()
-    {
-      double max = Math.Pow(F[0][0], 2);
-      for (int i = 0; i <= 21; i++)
-      {
-        for (int j = 0; j <= 21; j++)
-        {
-          if (Math.Pow(F[i][j], 2) > max) max = Math.Pow(F[i][j], 2);
+          if (Inm[i][j].Re > maxI.Re) maxI.Re = Inm[i][j].Re;
         }
       }
 
-      for (int i = 0; i <= 21; i++)
+      for (int i = 0; i < Inm.Count; i++)
       {
-        for (int j = 0; j <= 21; j++)
+        for (int j = 0; j < Inm[i].Count; j++)
         {
-          F[i][j] = Math.Pow(F[i][j], 2) / max;
+          Inm[i][j].Re /= maxI.Re;
+
         }
       }
     }
-
-    public static void PrintResultImn()
-    {
-      FindF();
-      NormalizeF();
-      NormalizeImn();
-    }
-
 
     public static AntennasRadiationPatternProblemResult Calculate(Problem problem)
     {
@@ -334,35 +260,48 @@ namespace Coursework.Services.Algorithms
       c1 = problem.C1;
       c2 = problem.C2;
       eps = problem.Eps;
-
-      CalculateImn();
-
-      double[][] arF = new double[22][];
-      for (int i = 0; i < 22; i++)
+      Inm.Clear();
+      matrixF.Clear();
+      matrixFNext.Clear();
+      /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      Splitting();
+      InitializeF();
+      GetNextF();
+      int ci = 0;
+      while (!checkForСonvergence(matrixF, matrixFNext))
       {
-        arF[i] = new double[22];
+        ci++;
+        GetNextF();
+        if (ci == 10)
+        {
+          break;
+        }
       }
 
-      for (int i = 0; i < 22; i++)
-        for (int j = 0; j < 22; j++)
-          arF[i][j] = F[i][j];
-      //for (int i = 10; i >=0; i--)
-      //    for (int j = 10; j >=0; j--)
-      //      arF[10-i][10-j] = F[i][j];
 
-      //for (int i = 21; i > 11; i--)
-      //  for (int j = 10; j >= 0; j--)
-      //    arF[33 - i][10 - j] = F[i][j];
+      //find Inm
+      ///////////////////////////////////////////////////////////////////////////
+      for (int n = 0; n <= 2 * N + 1; n++)
+      {
+        Inm.Add(new List<ComplexNumbers>());
+        for (int m = 0; m <= 2 * M + 1; m++)
+        {
+          Inm[n].Add(CalculateIntegralI(n - N, m - M));
+        }
+      }
+      NormalizeI();
 
-      //for (int i = 10; i >= 0; i--)
-      //  for (int j = 21; j > 11; j--)
-      //    arF[10 - i][33 - j] = F[i][j];
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-      //for (int i = 21; i > 11; i--)
-      //  for (int j = 21; j > 11; j--)
-      //    arF[33 - i][33 - j] = F[i][j];
+      double[][] arF = new double[SplitNumberForIntegral + 1][];
+      for (int i = 0; i <= SplitNumberForIntegral; i++)
+      {
+        arF[i] = new double[SplitNumberForIntegral + 1];
+      }
 
-      arF[11][11] = F[11][11];
+      for (int i = 0; i <= SplitNumberForIntegral; i++)
+        for (int j = 0; j <= SplitNumberForIntegral; j++)
+          arF[i][j] = matrixF[i][j].Re;
 
       double[][] arI = new double[2 * N + 1][];
       for (int i = 0; i < 2 * N + 1; i++)
@@ -370,13 +309,29 @@ namespace Coursework.Services.Algorithms
         arI[i] = new double[2 * M + 1];
       }
 
+      double[] Ix = new double[2 * N + 1];
+      double[] Iy = new double[2 * M + 1];
       for (int i = 0; i < 2 * N + 1; i++)
+      {
+        Ix[i] = i - N;
         for (int j = 0; j < 2 * M + 1; j++)
-          arI[i][j] = Imn[i][j].Re;
+        {
+          arI[i][j] = Inm[i][j].Re;
+        }
+      }
+
+      for (int i = 0; i < 2 * M + 1; i++)
+      {
+        Iy[i] = i - M;
+      }
 
       return new AntennasRadiationPatternProblemResult
       {
         F = arF,
+        Fx = ksi1,
+        Fy = ksi2,
+        Ix = Ix,
+        Iy = Iy,
         I = arI
       };
 
